6bf8902b7e888b18dcca6d6182deb15745270cec
MarioJunior01 <mario.junior6@estudante.ifms.edu.br>
2025-11-07 14:58:46 -0400

    Atividade 1 finalizada

--- meuPrimeiroApp/src/views/Tab1Page.vue
+++ meuPrimeiroApp/src/views/Tab1Page.vue
@@ -2,22 +2,71 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Imagens Bonitinhas</ion-title>
       </ion-toolbar>
     </ion-header>
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
+        <ion-toolbar> </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+      <IonHeader />
+      <IonCard v-for="img in imagens">
+        <img :src="img.download_url" />
+        <ion-card-title>
+          {{img.author }}
+        </ion-card-title>
+      </IonCard>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+} from "@ionic/vue";
+import { ref } from "vue";
+
+export default {
+  name: "ImagesBonitinhas ",
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+  },
+  setup() {
+    const imagens = ref();
+
+    async function pegarImagens() {
+      let resp = await fetch("https://picsum.photos/v2/list");
+      let respJson = await resp.json();
+      imagens.value = respJson;
+    }
+    return {
+      imagens,
+      pegarImagens,
+    };
+  },
+
+  mounted() {
+    this.pegarImagens();
+  }
+}
 </script>
